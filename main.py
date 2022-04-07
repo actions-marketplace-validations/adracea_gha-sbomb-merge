fd710c42de3b12817f0ad007ff368a53dbeec6b4
Alexandru Dracea <adracea@gmail.com>
2022-04-07 20:40:39 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,13 @@
 import xml.etree.ElementTree as ET
-import os, os.path, sys
+import os
+import os.path
 import glob
 
+
 def main():
-    ns = {'default':'http://cyclonedx.org/schema/bom/1.3'}
-    act_input = os.environ["INPUT_FILES"] # "./bom*.xml"
-    act_input2 = os.environ["INPUT_OFILE"] # "finalBom.xml"
+    ns = {'default': 'http://cyclonedx.org/schema/bom/1.3'}
+    act_input = os.environ["INPUT_FILES"]  # "./bom*.xml"
+    act_input2 = os.environ["INPUT_OFILE"]  # "finalBom.xml"
     files = glob.glob(act_input)
     print("Found the following files:")
     print(files)
@@ -14,24 +16,28 @@ def main():
     rootTools = []
     rootMetaComps = []
     for fil in files[1:]:
-        root1 = ET.parse(fil).getroot().find('default:components',ns).findall('default:component',ns)
-        root2 = ET.parse(fil).getroot().find('default:metadata',ns).find('default:tools',ns).findall('default:tool',ns)
-        root3 = ET.parse(fil).getroot().find('default:metadata',ns).findall('default:component',ns)
+        root1 = ET.parse(fil).getroot().find('default:com' +
+                'ponents', ns).findall('default:component', ns)
+        root2 = ET.parse(fil).getroot().find('default:meta' +
+                'data', ns).find('default:tools', ns).findall('default:tool', ns)
+        root3 = ET.parse(fil).getroot().find('default:meta' +
+                'data', ns).findall('default:component', ns)
         rootComps.extend(root1)
         rootTools.extend(root2)
         rootMetaComps.extend(root3)
     for element in rootComps:
-        tree.getroot().find('default:components',ns).append(element)
+        tree.getroot().find('default:components', ns).append(element)
     for element in rootTools:
-        tree.getroot().find('default:metadata',ns).find('default:tools',ns).append(element)
+        tree.getroot().find('default:metadata', ns).find('default:' +
+                'tools', ns).append(element)
     for element in rootMetaComps:
-        tree.getroot().find('default:metadata',ns).append(element)
+        tree.getroot().find('default:metadata', ns).append(element)
     tree.write(f"./{act_input2}")
     strfile= ""
     with open(f"./{act_input2}") as file:
-        strfile = file.read().replace("ns0:","").replace(":ns0","")
+        strfile = file.read().replace("ns0:", "").replace(":ns0", "")
         strfile = '<?xml version="1.0" encoding="UTF-8"?>' + strfile
-    with open(f"./{act_input2}","w") as file:
+    with open(f"./{act_input2}", "w") as file:
         file.write(strfile)
     print(f"Finished merging SBOMs into {act_input2}")
     print(f'::set-output name=success::{act_input2}')
